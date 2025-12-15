57f287e885eadf160d049025a8b3c1a8ed546d73
mukta-avhad <you@example.com>
2025-12-15 13:43:53 +0530

    Fix login flow and add auth callback page

--- app/auth/page.jsx
+++ app/auth/page.jsx
@@ -1,49 +1,55 @@
-"use client"
-import React from 'react'
-import Image from 'next/image'
-import { Button } from '@/components/ui/button'
-import { supabase } from '@/services/supabaseClient'
+"use client";
+import React from "react";
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { supabase } from "@/services/supabaseClient";
 
 function Login() {
+  const signInWithGoogle = async () => {
+    const { error } = await supabase.auth.signInWithOAuth({
+      provider: "google",
+      options: {
+        prompt: "select_account", // forces email chooser
+        redirectTo: `${window.location.origin}/auth/callback`, // redirect after login
+      },
+    });
 
-    const signInWithGoogle=async()=>{
-        const {error}=await supabase.auth.signInWithOAuth({
-            provider:'google',
-             options: {
-      prompt: "select_account",
-      redirectTo: `${window.location.origin}/auth/callback`
+    if (error) {
+      console.error("Error:", error.message);
     }
-        
-        });
-        if(error)
-        {
-            console.error('Error:',error.message);
-        }
-    };
+  };
 
-
-    return (
-        <div className='flex flex-col items-center justify-center h-screen '>
-            <div className='flex flex-col items-center border rounded-2xl p-8'>
-                <Image src={'/logo.jpeg'} alt='logo'
-                   width={400}
-                   height={100}
-                   className='w-[180px]'
-            />
-            <div>
-                <Image src={'/login.jpeg'} alt='login'
-                   width={600}
-                   height={400}
-                   className='w-[400px] h-[250px] rounded-2xl'
-                />
-                <h2 className='text-2xl font-bold text-center mt-5'>Welcome to AiCruiter</h2>
-                <p className='text-gray-500 text-center'>Sign In with Google Authentication</p>
-                <Button className='mt-5 w-full'
-                onClick={signInWithGoogle}
-                >Login with Google</Button>
-            </div>
-            </div>
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <div className="flex flex-col items-center border rounded-2xl p-8">
+        <Image
+          src={"/logo.jpeg"}
+          alt="logo"
+          width={400}
+          height={100}
+          className="w-[180px]"
+        />
+        <div>
+          <Image
+            src={"/login.jpeg"}
+            alt="login"
+            width={600}
+            height={400}
+            className="w-[400px] h-[250px] rounded-2xl"
+          />
+          <h2 className="text-2xl font-bold text-center mt-5">
+            Welcome to AiCruiter
+          </h2>
+          <p className="text-gray-500 text-center">
+            Sign In with Google Authentication
+          </p>
+          <Button className="mt-5 w-full" onClick={signInWithGoogle}>
+            Login with Google
+          </Button>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
-export default Login
+
+export default Login;
